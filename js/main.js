d816314f162a10f4af7d690e17ea1a8509c334e5
Esskedda <69585155+Esskedda@users.noreply.github.com>
2021-02-06 01:16:51 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,54 @@
+$(document).ready(function () {
+  const flowersSlider = new Swiper('.flowers-slider', {
+  // Параметр слайдера
+  loop: true,
+  slidesPerView: 6,
+
+  //  Подключаем кнопки
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+const reviewsSlider = new Swiper('.reviews-slider', {
+  // Параметр слайдера
+  loop: true,
+  slidesPerView: 1,
+
+  //  Подключаем кнопки
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  
+});
+
+$("#review-1").on('click', function() {
+
+  $.fancybox.open([
+    {
+      src  : 'https://source.unsplash.com/IvfoDk30JnI/1500x1000',
+      opts : {
+        caption : 'First caption',
+        thumb   : 'https://source.unsplash.com/IvfoDk30JnI/240x160'
+      }
+    },
+    {
+      src  : 'https://source.unsplash.com/0JYgd2QuMfw/1500x1000',
+      opts : {
+        caption : 'Second caption',
+        thumb   : 'https://source.unsplash.com/0JYgd2QuMfw/240x160'
+      }
+    }
+  ], {
+    loop : true,
+    thumbs : {
+      autoStart : true
+    }
+  });
+
+});
+});
+
